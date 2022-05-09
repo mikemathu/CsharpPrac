1f6f0d59e167c04c60a2db2cc6aefa43d85997b6
Michael Mathu <mathumichael17@gmail.com>
2022-05-09 20:36:30 +0300

    Array decraration ans creation

---
+++ array/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Arrays
+{
+    class Arrays
+    {
+        static void Main(string[] args)
+        {
+            // Array Decraration
+            //int[] scores;
+
+            //Array creation
+            //int[] scores = new int[] { 2, 3, 6 };
+
+            //int lengthInArray = scores.Length;
+
+           // Console.WriteLine(scores[1]);
+        }
+    }
+}
