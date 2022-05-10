a37dcc418d18b0d7916bb9d4212f8358f90cb51c
Michael Mathu <mathumichael17@gmail.com>
2022-05-10 20:34:24 +0300

    average of an array using foreachloop

--- foreach/Program.cs
+++ foreach/Program.cs
@@ -11,13 +11,27 @@
             //Minimum Value
             //foreach (int array in arrays )
             //{
-                //if (array < currentMinimum)
-                //{
-                    //currentMinimum = array;
-                //}
+            //if (array < currentMinimum)
+            //{
+            //currentMinimum = array;
+            //}
             //}
 
             //Console.WriteLine(currentMinimum);
+
+            //AVERAGE
+            int[] arrays = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+            int total = 0;
+
+            foreach (int array in arrays)
+            {
+                total += array;
+            }
+
+            float average = (float)total / arrays.Length;
+
+            Console.WriteLine(average);
         }
     }
 }
