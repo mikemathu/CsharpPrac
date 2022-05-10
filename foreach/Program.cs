4bfe9341508d61a68da1a8c14187ccc14a1544eb
Michael Mathu <mathumichael17@gmail.com>
2022-05-10 20:25:36 +0300

    minimum value using foreach loop

--- foreach/Program.cs
+++ foreach/Program.cs
@@ -4,20 +4,20 @@
     {
         static void Main(string[] args)
         {
-            int[] arrays = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            //int[] arrays = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
 
-            int currentMinimum = Int32.MaxValue;
+            //int currentMinimum = Int32.MaxValue;
 
             //Minimum Value
-            foreach (int array in arrays )
-            {
-                if (array < currentMinimum)
-                {
-                    currentMinimum = array;
-                }
-            }
+            //foreach (int array in arrays )
+            //{
+                //if (array < currentMinimum)
+                //{
+                    //currentMinimum = array;
+                //}
+            //}
 
-            Console.WriteLine(currentMinimum);
+            //Console.WriteLine(currentMinimum);
         }
     }
 }
