f35277ce160a86de2d622ca93e02fb96171dad72
Michael Mathu <mathumichael17@gmail.com>
2022-05-11 20:04:42 +0300

    initial way

--- EvenAndOddDecisionMaking/Program.cs
+++ EvenAndOddDecisionMaking/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EvenAndOdd
+{
+    public class EvenAndOdd
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter Number");
+           string numberAsText = Console.ReadLine();
+
+           int number = Convert.ToInt32(numberAsText);
+
+            int modulus = number % 2;
+
+            if (modulus == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+
+        
+
+
+        }
+    }
+}
