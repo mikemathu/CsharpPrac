5ca7be1ccfadef24d8627d31c7929fcfd857beb7
Michael Mathu <mathumichael17@gmail.com>
2022-05-11 05:35:37 +0300

    push commit

--- Bool/Program.cs
+++ Bool/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Bool
+{
+    public class Bool
+    {
+        static void Main(string[] args)
+        {
+            int score = 100;
+
+            int pointsNeededToPass = 100;
+
+            //The initial way
+            //bool levelComplete;
+
+
+            //if (score >= pointsNeededToPass)            
+            //levelComplete = true;            
+            //else            
+            //levelComplete = false;            
+
+            // Console.WriteLine(levelComplete);
+
+            //Improved way
+            bool levelComplete = (score >= pointsNeededToPass);
+
+            if (levelComplete)
+                Console.WriteLine("You have beten the level");
+            else
+                Console.WriteLine("Try next time");
+
+        }
+    }
+}
