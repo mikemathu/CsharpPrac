9752fec6150b9e77e00254918c07f3494c0da2aa
Michael Mathu <mathumichael17@gmail.com>
2022-05-11 06:19:06 +0300

    switch statement calsculator

--- Switch Statement/Program.cs
+++ Switch Statement/Program.cs
@@ -11,18 +11,34 @@
             switch (grade)
             {
                 case >= 80:
-                    Console.WriteLine("Your grade is A");
+                    //Console.WriteLine("Your grade is A");
                     break;
                 case >= 60:
-                    Console.WriteLine("Your grade is B");
+                    //Console.WriteLine("Your grade is B");
                     break;
                 case >= 50:
-                    Console.WriteLine("Your grade is C");
+                    //Console.WriteLine("Your grade is C");
                     break;
                 default:
-                    Console.WriteLine("You failed");
+                   //Console.WriteLine("You failed");
                     break;
             }
+
+            //CALCULATOR
+            Console.WriteLine("Enter the first number");
+            string firstNumner = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstNumner);
+
+            Console.WriteLine("Enter Second Numner");
+            string secondNumner = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secondNumner);
+
+            Console.WriteLine("Enter the computation sign");
+            string computationalSign = Console.ReadLine();
+
+            int answer = firstNumber + secondNumber;
+
+            Console.WriteLine("Your number is {0}", answer);
         }
     }
 }
