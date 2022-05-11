00ffe69f311836edcc3c33c4c35eed27406e201f
Michael Mathu <mathumichael17@gmail.com>
2022-05-11 04:31:28 +0300

    defined class ball

--- Class creation/Program.cs
+++ Class creation/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Class_Ball
+{
+    class Ball
+    {
+        //Insatant variables/fields
+        private int size;
+        private int color;
+        private int thrown;
+
+        //constructor
+        public Ball(int size, int color, int thrown)
+        {
+            this.size = size;
+            this.color = color;
+            this.thrown = thrown;
+        }
+
+        //method
+        public int Pop()
+        {
+            size = 0;
+            return size;
+        }
+
+        public int Thrown()
+        {
+            if (size == 0)
+            {
+                this.thrown += 1;
+            }
+            return thrown;
+        }
+
+        public int Times()
+        {
+
+        }
+
+
+
+    }
+}
