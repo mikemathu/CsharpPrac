d3cea8d4266e6f6af803235b408bbb76756de5fb
Michael Mathu <mathumichael17@gmail.com>
2022-05-11 06:05:18 +0300

    switch grading system

---
+++ Switch Statement/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SwitchStatement
+{
+    public class SwitchStatement
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EnterYour Score");
+            string score = Console.ReadLine();
+            int grade = Convert.ToInt32(score);
+
+            switch (grade)
+            {
+                case >= 80:
+                    Console.WriteLine("Your grade is A");
+                    break;
+                case >= 60:
+                    Console.WriteLine("Your grade is B");
+                    break;
+                case >= 50:
+                    Console.WriteLine("Your grade is C");
+                    break;
+                default:
+                    Console.WriteLine("You failed");
+                    break;
+            }
+        }
+    }
+}
