35a0081b77b8ea92f56587020eb237f5de72b6f6
Michael Mathu <mathumichael17@gmail.com>
2022-05-11 04:45:30 +0300

    defined class color

---
+++ Class creation/Ball.cs
@@ -0,0 +1,42 @@
+﻿namespace Class_Ball
+{
+    class Ball
+    {
+        //Insatant variables/fields
+        private int size;
+        private int color;
+        private int thrown;
+
+        //constructor
+        public Ball(int size, int color, int thrown)
+        {
+            this.size = size;
+            this.color = color;
+            this.thrown = thrown;
+        }
+
+        //method
+        public int Pop()
+        {
+            size = 0;
+            return size;
+        }
+
+        public int Thrown()
+        {
+            if (size == 0)
+            {
+                this.thrown += 1;
+            }
+            return thrown;
+        }
+
+        public int Times()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Class creation/Color.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_creation
+{
+    internal class Color
+    {
+        private int red;
+        private int green;
+        private int blue;
+        private int alpha;
+
+        public Color(int red, int green, int blue, int alpha)
+        {
+            
+        }
+
+        public Color(int red, int green, int blue)
+        {
+            const int alpha = 255;
+        }
+
+        public int GetColor()
+        {
+            return red;
+        }
+
+        public void SetColor(int red, int green, int blue, int alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+
+        public int GrayScale()
+        {
+            int average = (this.red + this.green + this.blue) / 3;
+            return average;
+        }
+    }
+}
