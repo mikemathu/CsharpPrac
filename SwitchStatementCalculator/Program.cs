e0e769847e4eefd2cd4722471577b50c6f1729df
Michael Mathu <mathumichael17@gmail.com>
2022-05-12 08:39:22 +0300

    push commit

---
+++ SwitchStatementCalculator/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace SwitchStatementCalcultor
+{
+    public class SwitchStatementCalculator
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter first number");
+                string firstNumberInString = Console.ReadLine();
+                int firstNumber = Convert.ToInt32(firstNumberInString);
+
+                Console.WriteLine("Enter operation sign");
+                string operationSign = Console.ReadLine();
+
+                Console.WriteLine("Enter second number");
+                string secondNumberInString = Console.ReadLine();
+                int secondNumber = Convert.ToInt32(secondNumberInString);
+
+                switch (operationSign)
+                {
+                    case "+":
+                        int sum = firstNumber + secondNumber;
+                        Console.WriteLine(sum);
+                        break;
+                    case "-":
+                        int difference = firstNumber - secondNumber;
+                        Console.WriteLine(difference);
+                        break;
+                    case "*":
+                        int product = firstNumber * secondNumber;
+                        Console.WriteLine(product);
+                        break;
+                    case "/":
+                        float quotient = (float)firstNumber / secondNumber;
+                        Console.WriteLine(firstNumber + operationSign + secondNumber +" = "+ quotient);
+                        break;
+                    case "%":
+                        int remaider = firstNumber % secondNumber;
+                        Console.WriteLine(remaider);
+                        break;
+                    case "^":
+                        double power = Math.Pow(firstNumber, secondNumber);
+                        Console.WriteLine(power);
+                        break;
+                }
+            }
+           
+
+
+        }
+    }
+}
