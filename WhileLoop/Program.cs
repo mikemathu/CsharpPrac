3e179888659bb98a36337fe8daf857d236c7a4e4
Michael Mathu <mathumichael17@gmail.com>
2022-05-12 09:00:40 +0300

    while loop

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace WhileLoop
+{
+    public class WhileLoop
+    {
+        static void Main(String[] args)
+        {
+            int x = 1;
+
+            while (x <= 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+
+       
+    }
+}
