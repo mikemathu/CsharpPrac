58bfb9d73bb2eb7260fda31b52cdeebec881e9d1
Michael Mathu <mathumichael17@gmail.com>
2022-05-12 11:33:01 +0300

    FizzBuzz challenge

---
+++ FuzzBuzz Challenge/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FuzzBuzzChallenge
+{
+    public class FuzzBuzz
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 100; number++ )
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    continue;
+                }
+
+                if (number%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    continue;
+                }
+
+                if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    continue;
+                }
+
+                
+
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
