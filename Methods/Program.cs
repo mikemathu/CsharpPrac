9bef2977afe61dcbca36d9c46b6bbf38b2606247
Michael Mathu <mathumichael17@gmail.com>
2022-05-12 22:39:09 +0300

    sequential code execution

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -5,15 +5,28 @@
         //Calling a static method
         static void Main(string[] args)
         {
-           CountToTen();
+            // CountToTen();
+            //}
+
+            //static void CountToTen()
+            //{
+            //for(int index = 1; index < 10; index++)
+            //Console.WriteLine(index);
+            //}
+
+            //SEQUENTIAL CODE EXECUTION
+            Console.WriteLine("I am about to go into a mwthod");
+
+            DoSomethingAwesome();
+
+            Console.WriteLine("I am back from the method");
+
         }
 
-        static void CountToTen()
+        static void DoSomethingAwesome()
         {
-            for(int index = 1; index < 10; index++)
-                Console.WriteLine(index);
+            Console.WriteLine("I am inside of a method! Awesome");
+
         }
-
-
-    }
+    } 
 }
