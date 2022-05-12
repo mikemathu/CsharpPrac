12f2fea0ced94739fca20fbece261b32b063b733
Michael Mathu <mathumichael17@gmail.com>
2022-05-12 19:25:45 +0300

    push commit

---
+++ OOP/Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Book
+    {
+        //Instant variables
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        //constructor
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        public Book(string title, string author, int pages, int wordCount)
+        {
+            this.title = title;
+            this.author =author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+        }
+
+        //Methods
+        //Retrive the current title of the book
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public string GetAuthor()
+        {
+            return author;
+        }
+
+        //Specify the new title for the book
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+
+        //Supply the text of the book and update the word count ccordingly
+        public void AssignWordCountFromText(string text)
+        {
+            wordCount = text.Split(' ').Length;
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using OOP;
+
+namespace CreatingClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //New Instant of the object(1st constructor)
+            //Book book = new Book("Fastlane", "MJ");
+
+            //Console.WriteLine(book.GetTitle());
+            //Console.WriteLine(book.GetAuthor());
+
+            //2nd Constructor
+            Book book = new Book("Think and Grow rich","Unknown", 200, 5000 );
+            //Console.WriteLine(book.GetTitle());
+            //Console.WriteLine(book.GetAuthor());
+
+            book.SetTitle("How to get rich");
+            Console.WriteLine(book.GetTitle());
+
+        }
+    }
+}
