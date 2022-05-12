e4036964a5da40b01459fa9ff3c5a2e4e1d76920
Michael Mathu <mathumichael17@gmail.com>
2022-05-12 21:44:52 +0300

    calling static method

---
+++ Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Methods
+{
+    public class Methods
+    {
+        //Calling a static method
+        static void Main(string[] args)
+        {
+           CountToTen();
+        }
+
+        static void CountToTen()
+        {
+            for(int index = 1; index < 10; index++)
+                Console.WriteLine(index);
+        }
+
+
+    }
+}
