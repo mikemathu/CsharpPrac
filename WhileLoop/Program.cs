31eb2790d46a147309475bd0206433aeafd7956d
Michael Mathu <mathumichael17@gmail.com>
2022-05-12 09:31:39 +0300

    do while number loop

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -4,13 +4,40 @@
     {
         static void Main(String[] args)
         {
-            int x = 1;
+            //int x = 1;
 
-            while (x <= 10)
+            //while (x <= 10)
+            //{
+            //Console.WriteLine(x);
+            //x++;
+            //}
+
+            //LOOP FOR ENTERING NUMBER BTWN 0 AND 10
+
+            //int playerNumber = -1;
+
+            // while (playerNumber < 0 || playerNumber > 10)
+            //{ 
+            //Console.WriteLine("Enter Number btwn 0 and 10");
+            //string numberInText = Console.ReadLine();
+            //playerNumber = Convert.ToInt32(numberInText);
+            //}
+
+            //DO WHILE LOOP
+            int playerNumber;
+            do
             {
-                Console.WriteLine(x);
-                x++;
+                Console.WriteLine("Enter number between 0 and 10");
+                string palyerResponce = Console.ReadLine();
+                playerNumber = Convert.ToInt32(palyerResponce);
             }
+            while (playerNumber < 0 || playerNumber > 10);
+
+     
+
+
+
+
         }
 
        
