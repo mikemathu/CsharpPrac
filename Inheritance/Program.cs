5ee914cd3406b7682c0ff6ae25f6ff24d8b3fea1
Michael Mathu <mathumichael17@gmail.com>
2022-05-13 12:48:33 +0300

    push commit

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,8 +1,19 @@
-﻿namespace Inheritance
+﻿using Inheritance;
+
+namespace Inheritance
 {
     //Base Class
     class Polygon
     {
+        static void Main(string[] args)
+        {
+            //Polygon polygon = new Polygon();
+            Square square = new Square(3.3f);
 
+            //polygon.Size = 3.3f;
+            //square.Size = 3.3f;
+
+            Console.WriteLine(square.Size);
+        }        
     }
 }
--- Inheritance/Square.cs
+++ Inheritance/Square.cs
@@ -8,6 +8,14 @@ namespace Inheritance
 {
     internal class Square : Polygon
     {
-        
+        public float Size { get; set; }
+
+        public Square(float size) //Use the Polygon(int numberOfSides) constructor in the base class
+        {
+            Size = size;
+           // NumberOfSides = 4;
+
+            //Console.WriteLine("The sides of the square are" + Size);
+        }
     }
 }
