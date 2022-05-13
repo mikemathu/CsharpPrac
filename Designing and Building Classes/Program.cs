b6c1bf1b34a37994c2470326d46f0278e8cd5f24
Michael Mathu <mathumichael17@gmail.com>
2022-05-13 14:45:49 +0300

    added color class

---
+++ Designing and Building Classes/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace BuildingClasses
+{
+    public class Color
+    {
+        //Instant variables
+        private int red;
+        private int green;
+        private int blue;
+        private int alpha;
+
+        public Color(int red, int green, int blue, int alpha)
+        {
+
+        }
+
+        public Color(int red, int green, int blue)
+        {
+            this.alpha = 255;
+        }
+
+        public int GetRed()
+        {
+            return red;
+        }
+        public int GetGreen()
+        {
+            return green;
+        }
+
+        public int GetBlue()
+        {
+            return blue;
+        }
+
+        public int GetAlpha()
+        {
+            return alpha;
+        }
+
+        public int GetAverage()
+        {
+            return (red + green + blue) / 3;
+        }
+    }
+}
