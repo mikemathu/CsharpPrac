b94f3fe88b50f14f3a49b384b9769ded5e714226
Michael Mathu <mathumichael17@gmail.com>
2022-05-13 10:54:59 +0300

    get and set using methods

---
+++ Properties/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Book
+    {
+        //Instant variable
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        //Constructor
+        public Book(string title, string author)
+        {
+            //class level title = method level title
+            this.title = title;
+            this.author = author;
+        }
+
+        public Book(string title, string author, int pages, int wordCount)
+        {
+            //class level title = method level title
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+        }
+
+        //Method
+        public string GetTitle()
+        {
+            return title;
+        }
+        //Specify the new titile for the new book
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+
+
+
+
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using Properties;
+
+namespace Properties
+{
+    public class Property
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("how to get rich", "unknown");
+
+            //Console.WriteLine(book.GetTitle());
+
+            book.SetTitle("New title");
+
+            Console.WriteLine(book.GetTitle());
+
+
+
+            
+        }
+    }
+}
